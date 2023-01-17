060ac5379fb7f4974ba3b748dc88d26fae89987f
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2023-01-17 17:04:24 +0200

    Fixed demo link

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,12 @@
 import carsFromServer from './api/cars';
 import colorsFromServer from './api/colors';
+import { FullCar } from './types';
 
 export const getColorById = (colorId: number) => (
   colorsFromServer.find(color => color.id === colorId)
 );
 
-export const getFullCar = () => {
+export const getFullCar = (): FullCar[] => {
   return carsFromServer.map(car => ({
     ...car,
     color: getColorById(car.colorId),
